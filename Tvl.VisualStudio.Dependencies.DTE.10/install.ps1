94946bd822c3bf887d2d31c03c6a8a22ebde4e42
Sam Harwell <sam.harwell@rackspace.com>
2014-02-27 08:48:23 -0600

    Need to include stdole.dll in the DTE package

--- Tvl.VisualStudio.Dependencies.DTE.10/install.ps1
+++ Tvl.VisualStudio.Dependencies.DTE.10/install.ps1
@@ -4,7 +4,7 @@ foreach ($reference in $project.Object.References)
 {
 	switch -regex ($reference.Name.ToLowerInvariant())
 	{
-	"^envdte(?:|80|90|90a|100)$"
+	"^(?:stdole|envdte(?:|80|90|90a|100))$"
 		{
 			$reference.CopyLocal = $false;
 			$reference.EmbedInteropTypes = $false;
