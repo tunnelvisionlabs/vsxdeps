c12278654b5b1454f28b7cafca455f980ad6193a
Sam Harwell <sam.harwell@rackspace.com>
2014-02-27 08:12:38 -0600

    Create initial build script and metadata packages for each supported version of Visual Studio

---
+++ build-2012.ps1
@@ -0,0 +1,20 @@
+param([string]$MSEnv = 'C:\Program Files (x86)\Common Files\microsoft shared\MSEnv')
+
+$nuget = '.\.nuget\NuGet.exe'
+$Version = '11.0.0-alpha001'
+$OutDir = 'packages-2012'
+
+$packages = @(
+	'Tvl.VisualStudio.Dependencies.11'
+)
+
+# Create the output folder if it doesn't exist
+if (!(Test-Path $OutDir))
+{
+	mkdir $OutDir | Out-Null
+}
+
+foreach ($package in $packages)
+{
+	&$nuget pack "$package\$package.nuspec" -Version $Version -OutputDirectory $OutDir -Prop MSEnv=$MSEnv
+}
---
+++ build-all.ps1
@@ -0,0 +1,3 @@
+./build-2010.ps1
+./build-2012.ps1
+./build-2013.ps1
