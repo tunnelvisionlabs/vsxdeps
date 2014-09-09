4bba7d6ccc07979bb2e3463d641899fb7c116962
Sam Harwell <sam.harwell@rackspace.com>
2014-09-09 06:31:55 -0500

    Use the Microsoft.VisualStudio.Shell.Interop.8.0 assembly from the Visual Studio 2008 SDK (fixes #11)

--- build-2005.ps1
+++ build-2005.ps1
@@ -1,6 +1,7 @@
 param(
 	[string]$MSEnv = 'C:\Program Files (x86)\Common Files\microsoft shared\MSEnv',
 	[string]$VSSDK = 'C:\Program Files (x86)\Visual Studio 2005 SDK',
+	[string]$VSSDK9 = 'C:\Program Files (x86)\Microsoft Visual Studio 2008 SDK',
 	[string]$VSIDE = 'C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE'
 )
 
@@ -35,5 +36,5 @@ if (!(Test-Path $OutDir))
 
 foreach ($package in $packages)
 {
-	&$nuget pack "$package\$package.nuspec" -Version $Version -OutputDirectory $OutDir -Prop MSEnv=$MSEnv -Prop VSSDK=$VSSDK -Prop VSIDE=$VSIDE
+	&$nuget pack "$package\$package.nuspec" -Version $Version -OutputDirectory $OutDir -Prop MSEnv=$MSEnv -Prop VSSDK=$VSSDK -Prop VSSDK9=$VSSDK9 -Prop VSIDE=$VSIDE
 }
